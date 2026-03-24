f8804249b1ecac4391acbd4b8a4afbb5ea8ef023
Archie Belarmino <110542367+acchiii@users.noreply.github.com>
2026-03-25 04:46:31 +0800

    Fix Static Files with Whitenoise

--- mabini_skyview/settings.py
+++ mabini_skyview/settings.py
@@ -5,6 +5,7 @@ Django settings for Mabini SkyView project.
 from pathlib import Path
 import os
 from decouple import config, Csv
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -59,11 +60,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mabini_skyview.wsgi.application'
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+        config('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
